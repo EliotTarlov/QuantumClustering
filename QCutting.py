76ce7dace63ad14454154176ad3ff221b79efc97
EliotTarlov <tarlove@reed.edu>
2023-04-13 13:01:36 -0700

    at least visualization is working well...

--- QCutting.py
+++ QCutting.py
@@ -12,6 +12,7 @@ def createRandomScattering(num_points,scale=1,x_trans=0,y_trans=0):
    '''random scattering of num_points scaled by scale, and translated by x_trans and y_trans'''
    return [(random()*scale+x_trans,random()*scale+y_trans) for _ in range(num_points)]
    
+
 def createTestClustering(clusters,max_num_points=9, max_dist_btw_clusters=4):
    '''create a set of points with a number of clustered together scatterings'''
    out=[]
@@ -20,19 +21,6 @@ def createTestClustering(clusters,max_num_points=9, max_dist_btw_clusters=4):
       out+=createRandomScattering(int(random()*max_num_points)+1,x_trans=max_dist_btw_clusters*random(),y_trans=max_dist_btw_clusters*random()) 
    return out
    
-#handle command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--points',type=str,help='List of points. To be given as the path to a file which contains a list of tuples eg [(1,2),(2,3),...,(2.1,.01)]. If empty list is given, random')
-parser.add_argument('-c','--clusters', default=2, type=int, help="number of clusters to divide the data into. If empty, 2 is default.")
-args = parser.parse_args()
-
-# Checking if the random option was given
-if not args.points:
-   points = createTestClustering(args.clusters)#4 random points distributed across 0,1
-else:
-   from ast import literal_eval
-   with open(args.points) as f:
-      points=literal_eval(f.read())#convert from a string into a list of tuples. God bless this function my code was _ugly_.
 
 def distance(point1,point2):
    '''distance between two points (tuples) of arbitrary dimension'''
@@ -44,17 +32,23 @@ def distance(point1,point2):
       sum_of_squares += diff ** 2
    return sum_of_squares**.5
 
-#create graph
-G = nx.Graph()
-G.add_nodes_from(list(range(len(points))))#each node corresponds to a point
-for origin,destination in combinations_with_replacement(range(len(points)),2): #create the complete graph
-   if origin!=destination: #no self loops
-      G.add_edge(origin,destination,weight=distance(points[origin],points[destination])) #connect each vertex to each other vertex with an edge where the weight is inversely proportional to the distance between them. It is inverse because we want to minimize, not maximize
 
-# Adjacency is essentially a matrix which tells you which nodes are connected. This matrix is given as a sparse matrix, so we need to
-# convert it to a dense matrix
-adjacency = nx.adjacency_matrix(G).todense()
-nqubits = len(points) #each qubit corresponds to a vertex in the graph and each boolean assignment for each point corresponds with what side of the cut the vertex is in. 
+
+
+
+def printClustering(G,solution):
+   '''prints a networkxgraph with associated positions colored according to a solution bitstring'''
+   colors=[]
+   for i in solution:
+      if int(i):
+         colors.append("blue")
+      else:
+         colors.append("pink")
+   nx.draw_networkx(G,nx.get_node_attributes(G,'pos'), node_color=colors,with_labels=True,edgelist=[])
+   plt.show()  
+   
+
+
 
 def mincut_obj(solution, graph):
     """Given a bit string as a solution, this function returns
@@ -69,7 +63,6 @@ def mincut_obj(solution, graph):
        if solution[i] != solution[j]:
           obj -= graph.get_edge_data(i,j)["weight"]
     return obj
-
 def compute_expectation(counts, graph):
     """Computes expectation value based on measurement results
     Args:
@@ -138,49 +131,74 @@ def get_expectation(G, shots=512):
     def execute_circ(theta):
         
         qc = createQAOACirc(G, theta)
-        counts = backend.run(qc, seed_simulator=10, 
-                             nshots=512).result().get_counts()
+        counts = backend.run(qc, seed_simulator=10,nshots=512).result().get_counts()
         
         return compute_expectation(counts, G)
     
     return execute_circ
     
+
+
+if __name__=="__main__":
+   #handle command line arguments
+   parser = argparse.ArgumentParser()
+   parser.add_argument('-p', '--points',type=str,help='List of points. To be given as the path to a file which contains a list of tuples eg [(1,2),(2,3),...,(2.1,.01)]. If no points are given, random')
+   #parser.add_argument('-c','--clusters', default=2, type=int, help="number of clusters to divide the data into. If empty, 2 is default.")
+   args = parser.parse_args()
+
+   # Checking if the random option was given
+   if not args.points:
+      points = createTestClustering(2) #args.clusters)#2 random clusters
+   else:
+      from ast import literal_eval
+      with open(args.points) as f:
+         points=literal_eval(f.read())#convert from a string into a list of tuples. God bless this function my code was *ugly*.
+
+
+   #create graph
+   G = nx.Graph()
+   for i in range(len(points)):
+      G.add_node(i,pos=points[i])
+   for origin,destination in combinations_with_replacement(range(len(points)),2): #create the complete graph
+      if origin!=destination: #no self loops
+         G.add_edge(origin,destination,weight=distance(points[origin],points[destination])) #connect each vertex to each 
+         #other vertex with an edge where the weight is inversely proportional to the distance between them. It is inverse 
+         #because we want to minimize, not maximize   
+       
     
-    
+   get_expectation(G)
+   res = minimize(get_expectation(G),[1.0, 1.0,1.0,1.0],method='COBYLA') #get_expectation returns the function execute_circ which takes in a 
+   #1D vector of even dimension. 
+   print(res)
+   backend = Aer.get_backend('aer_simulator')
+   backend.shots = 512
 
-expectation = get_expectation(G)
-res = minimize(expectation,
-               [1.0, 1.0],
-               method='COBYLA')
-backend = Aer.get_backend('aer_simulator')
-backend.shots = 512
+   # Adjacency is essentially a matrix which tells you which nodes are connected. This matrix is given as a sparse matrix, so we need to
+   # convert it to a dense matrix
+   adjacency = nx.adjacency_matrix(G).todense()
+   nqubits = len(points) #each qubit corresponds to a vertex in the graph and each boolean assignment for each point corresponds with what side of the cut the vertex is in. 
 
-qc_res = createQAOACirc(G, res.x)
+   qc_res = createQAOACirc(G, res.x)
+   createQAOACirc(G, res.x).draw()
+   plt.show()
+   plt.show()
+   counts =backend.run(qc_res, seed_simulator=10).result().get_counts()
+   #we need to sort to get the bar chart in decent order
+   sorted_counts = sorted(counts.items())
 
-counts =backend.run(qc_res, seed_simulator=10).result().get_counts()
-#we need to sort to get the bar chart in decent order
-sorted_counts = sorted(counts.items())
+   # extract the keys and values from the sorted dictionary
+   keys = [item[0] for item in sorted_counts]
+   values = [item[1] for item in sorted_counts]
 
-# extract the keys and values from the sorted dictionary
-keys = [item[0] for item in sorted_counts]
-values = [item[1] for item in sorted_counts]
-
-# create a bar chart from the sorted dictionary
-plt.bar(keys, values, align='center')
-#plt.xticks(range(len(counts)), list(counts.keys()))
-plt.show()
+   # create a bar chart from the sorted dictionary
+   plt.bar(keys, values, align='center')
+   plt.show()
 
 bestSolution= max(counts, key=counts.get)
-
-colors = [int(bestSolution[i]) for i in range(len(points))]
-
+printClustering(G,bestSolution)
 # create a scatter plot with colored points
-plt.scatter([p[0] for p in points], [p[1] for p in points], c=colors, cmap='cool', vmin=0, vmax=1)
-
-# show the plot
+#plt.scatter([p[0] for p in points], [p[1] for p in points], c=colors, cmap='cool', vmin=0, vmax=1)
 plt.show()
-
-colors = {node: int(bestSolution[node]) for node in G.nodes()}
 
 
 
